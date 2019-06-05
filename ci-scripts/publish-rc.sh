a1415f4c0576c2c87197204db996a988b92cc72f
hannah.van.seeters <hannah.van.seeters@sap.com>
2019-06-04 18:19:04 -0700

    my username

--- ci-scripts/publish-rc.sh
+++ ci-scripts/publish-rc.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-git config --global user.email "fundamental@sap.com"
-git config --global user.name "fundamental-bot"
+git config --global user.email "hannahvanseeters@gmail.com"
+git config --global user.name "hertweckhr1"
 
 npm run std-version -- --prerelease rc --no-verify
 
 git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH" > /dev/null 2>&1;
 
-cd lib 
+cd lib
 
 npm publish --tag prerelease
