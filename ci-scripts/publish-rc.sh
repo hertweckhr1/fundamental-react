060795627f2d09b6cff8890b8030d949669b5693
hannah.van.seeters <hannah.van.seeters@sap.com>
2019-06-05 08:33:19 -0700

    back to original

--- ci-scripts/publish-rc.sh
+++ ci-scripts/publish-rc.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-
+echo "starting"
 set -e
 
 git config --global user.email "hannahvanseeters@gmail.com"
@@ -8,7 +8,5 @@ git config --global user.name "hertweckhr1"
 npm run std-version -- --prerelease rc --no-verify
 
 git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH" > /dev/null 2>&1;
-
-cd lib
 
 npm publish --tag prerelease
