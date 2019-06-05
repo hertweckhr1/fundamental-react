b2ed71490047c6f070c116101d848f23ee845e2a
hannah.van.seeters <hannah.van.seeters@sap.com>
2019-06-05 08:57:20 -0700

    changed all to me

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-git config --global user.email "fundamental@sap.com"
-git config --global user.name "fundamental-bot"
+git config --global user.email "hannahvanseeters@gmail.com"
+git config --global user.name "hertweckhr1"
 
 # delete temp branch
 git push "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" ":$TRAVIS_BRANCH" > /dev/null 2>&1;
